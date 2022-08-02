56f269d88dcba1a579095bfc1f83358fde0d7edd
igorevanss <igorevans.pro@gmail.com>
2022-08-02 17:18:02 -0300

    implementação de estilos globais e do resetcss com styledcomponents

---
+++ src/assets/styles/GlobalStyles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  .root{
+    width: 100%;
+    height: 100%;
+  }
+`;
+export default GlobalStyle;
--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+import ResetStyle from '../assets/styles/ResetStyles'
+import GlobalStyle from '../assets/styles/GlobalStyles'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import Registration from './Registration'
@@ -5,6 +7,8 @@ import Registration from './Registration'
 export default function App() {
   return (
     <>
+    <ResetStyle />
+    <GlobalStyle />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
